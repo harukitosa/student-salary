49262b024d77b4f8bd5625e32cce097549f391c8
harukitosa <13haruki28@gmail.com>
2021-09-12 00:01:26 +0900

    lint

--- component/simpletable.tsx
+++ component/simpletable.tsx
@@ -1,11 +1,20 @@
-import { Text, Box, Grid, Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Stack } from "@chakra-ui/react";
+import {
+  Text,
+  Box,
+  Grid,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Stack,
+} from "@chakra-ui/react";
 import { workdata } from "../types/type";
 import Link from "next/link";
 import * as React from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import { useRouter } from "next/router";
 import { WorkData } from "../src/generated/graphql";
-
 
 const columns = [
   { field: "id", headerName: "ID", width: 30 },
@@ -59,47 +68,53 @@ export const DataTable = (props: { data: WorkData[] }) => {
       {props.data.slice(0, 30).map((item) => {
         return (
           <AccordionItem key={item.id}>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                <Stack direction="column" gridGap={0}>
-                  <Link href={`/workinfo/${item.name}`}>
-                  <a>
-                <Text fontSize="18" fontWeight="600" color="blue.400">{item.name}</Text>
-                </a>
-                </Link>
-                <Text as="span" fontSize="12">{item.workType}</Text>
-                </Stack>
-              </Box>
-              <Box flex="1" textAlign="left">
-                <Stack direction="column" gridGap={0}>
-                <Text fontSize="18" fontWeight="400">{item.type}</Text>
-                </Stack>
-              </Box>
-              <Box flex="1" textAlign="left">
-                <Stack direction="column" gridGap={0}>
-                <Text fontSize="18" fontWeight="400">{item.term}</Text>
-                </Stack>
-              </Box>
-              <Box flex="1" textAlign="center">
-                <Text fontSize="18">
-                {item.salary}円
-                </Text>
-              </Box>
-              {/* <AccordionIcon /> */}
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            <Stack>
-              <Text>経験年数:{item.experience}</Text>
-              <Text>週出勤日数:{item.workdays}</Text>
-              <Text fontSize="12">詳細</Text>
-              <Text>{item.detail}</Text>
-            </Stack>
-          </AccordionPanel>
-        </AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  <Stack direction="column" gridGap={0}>
+                    <Link href={`/workinfo/${item.name}`}>
+                      <a>
+                        <Text fontSize="18" fontWeight="600" color="blue.400">
+                          {item.name}
+                        </Text>
+                      </a>
+                    </Link>
+                    <Text as="span" fontSize="12">
+                      {item.workType}
+                    </Text>
+                  </Stack>
+                </Box>
+                <Box flex="1" textAlign="left">
+                  <Stack direction="column" gridGap={0}>
+                    <Text fontSize="18" fontWeight="400">
+                      {item.type}
+                    </Text>
+                  </Stack>
+                </Box>
+                <Box flex="1" textAlign="left">
+                  <Stack direction="column" gridGap={0}>
+                    <Text fontSize="18" fontWeight="400">
+                      {item.term}
+                    </Text>
+                  </Stack>
+                </Box>
+                <Box flex="1" textAlign="center">
+                  <Text fontSize="18">{item.salary}円</Text>
+                </Box>
+                {/* <AccordionIcon /> */}
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Stack>
+                <Text>経験年数:{item.experience}</Text>
+                <Text>週出勤日数:{item.workdays}</Text>
+                <Text fontSize="12">詳細</Text>
+                <Text>{item.detail}</Text>
+              </Stack>
+            </AccordionPanel>
+          </AccordionItem>
         );
       })}
-  </Accordion>
+    </Accordion>
   );
 };
