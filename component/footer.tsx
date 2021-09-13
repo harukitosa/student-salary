18b2163dbe3b507a67da2d96d5fc1b2e2e31c246
harukitosa <13haruki28@gmail.com>
2021-09-13 18:15:00 +0900

    lint

--- component/footer.tsx
+++ component/footer.tsx
@@ -6,11 +6,11 @@ import {
   useColorModeValue,
   SimpleGrid,
 } from "@chakra-ui/react";
-import Link from "next/link"
+import Link from "next/link";
 
 const ListHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
       {children}
     </Text>
   );
@@ -20,53 +20,57 @@ export const Footer = () => {
   return (
     <>
       <Box
-      mt="24"
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
-      <Container as={Stack} maxW={'6xl'} py={10}>
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
-          <Stack align={'flex-start'}>
-            <ListHeader>StudentSalary</ListHeader>
-            <Link href={'/'}>
-              <a>
-                ホーム
-              </a>
-            </Link>
-            <Link href={'/workinfo'}>
-              <a>
-              企業一覧
-              </a>
-            </Link>
-            <Link href={'/blog/all'}>
-              <a href="">ブログまとめ</a>
-            </Link>
-            <Link href={'/review'}>
-              <a href="">口コミ一覧</a>
-            </Link>
-          </Stack>
+        mt="24"
+        bg={useColorModeValue("gray.50", "gray.900")}
+        color={useColorModeValue("gray.700", "gray.200")}
+      >
+        <Container as={Stack} maxW={"6xl"} py={10}>
+          <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+            <Stack align={"flex-start"}>
+              <ListHeader>StudentSalary</ListHeader>
+              <Link href={"/"}>
+                <a>ホーム</a>
+              </Link>
+              <Link href={"/workinfo"}>
+                <a>企業一覧</a>
+              </Link>
+              <Link href={"/blog/all"}>
+                <a href="">ブログまとめ</a>
+              </Link>
+              <Link href={"/review"}>
+                <a href="">口コミ一覧</a>
+              </Link>
+            </Stack>
 
-          <Stack align={'flex-start'}>
-            <ListHeader>Contribute</ListHeader>
-            <Link href={'/new'}><a>時給登録</a></Link>
-            <Link href={'/review/new'}><a>口コミ登録</a></Link>
-            <Link href={'https://docs.google.com/forms/d/e/1FAIpQLSfuEid3qlioZLBR3-QjGh7fmNUvtcesd6EYpX5uxOh22h3--Q/viewform'}>
-              <a>ブログ登録</a>  
-            </Link>
-          </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Contribute</ListHeader>
+              <Link href={"/new"}>
+                <a>時給登録</a>
+              </Link>
+              <Link href={"/review/new"}>
+                <a>口コミ登録</a>
+              </Link>
+              <Link
+                href={
+                  "https://docs.google.com/forms/d/e/1FAIpQLSfuEid3qlioZLBR3-QjGh7fmNUvtcesd6EYpX5uxOh22h3--Q/viewform"
+                }
+              >
+                <a>ブログ登録</a>
+              </Link>
+            </Stack>
 
-          <Stack align={'flex-start'}>
-            <ListHeader>Contact</ListHeader>
-            <Link href={'https://twitter.com/tosa_now'}>開発者twitter</Link>
-          </Stack>
-        </SimpleGrid>
-      </Container>
-    </Box>
+            <Stack align={"flex-start"}>
+              <ListHeader>Contact</ListHeader>
+              <Link href={"https://twitter.com/tosa_now"}>開発者twitter</Link>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+      </Box>
     </>
-
   );
 };
 
-      /* <Box minH={20}></Box>
+/* <Box minH={20}></Box>
       <Box
         bg={useColorModeValue("gray.50", "gray.900")}
         color={useColorModeValue("gray.700", "gray.200")}
