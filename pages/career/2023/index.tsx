0057bb921ec11c59b95095747721e7405d1a4b24
harukitosa <13haruki28@gmail.com>
2021-10-03 20:58:35 +0900

    fix

--- pages/career/2023/index.tsx
+++ pages/career/2023/index.tsx
@@ -1,9 +1,5 @@
 import { Container } from "@chakra-ui/react";
 
 export default function Career2023Page() {
-  return (
-    <Container>
-      2023年度新卒採用ページ
-    </Container>
-  );
+  return <Container>2023年度新卒採用ページ</Container>;
 }
