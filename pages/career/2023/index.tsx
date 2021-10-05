ef96d0b67cfc7bf0ce4ca7e1d4d1abf913a1d5f7
harukitosa <13haruki28@gmail.com>
2021-10-05 17:37:07 +0900

    fix blog

--- pages/career/2023/index.tsx
+++ pages/career/2023/index.tsx
@@ -1,5 +1,115 @@
-import { Container } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Career2023Page() {
-  return <Container>2023年度新卒採用ページ</Container>;
+  return (
+  <Box w={{base: "100vw", md: "80vw"}} m="auto" mt="12">
+    <Text as="h1" fontSize="4xl" fontWeight="bold">
+    2023年度ソフトウェアエンジニア新卒採用ページ
+    </Text>
+    <Text fontSize="md">
+    23卒のソフトウェアエンジニアを募集している企業を一覧で掲載しています。
+    <br/>
+    記載されている情報は精査して掲載していますが、リンク先の変更による間違い等があるので必ず参照元のサイトをご確認ください。
+    </Text>
+
+<Text>
+応募が開始次第適宜収取追加していきます。
+* 管理者Twitter <a href="https://twitter.com/tosa_now">@tosa_now</a> に23新卒情報をご連絡いただければこちらに掲載します。
+</Text>
+
+
+
+
+
+<Text as="h2" fontSize="28" fontWeight="semibold">
+Voyage Group
+</Text>
+サマーインターンTreasureが大人気。
+
+
+
+新卒採用情報
+【第2回ES締切】10/25(月)AM9:00
+
+<a href="https://voyagegroup.snar.jp/jobboard/detail.aspx?id=u2mj1IfbZtc">
+https://voyagegroup.snar.jp/jobboard/detail.aspx?id=u2mj1IfbZtc
+</a>
+
+
+
+コーポレートページ
+https://voyagegroup.com/
+
+
+
+
+
+Visional
+ビズリーチが有名
+
+
+
+新卒採用情報
+https://hrmos.co/pages/hrmos/jobs/3100100100906
+
+
+
+コーポレートページ
+https://www.visional.inc/ja/index.html
+
+
+
+
+
+Cyberagent
+メディア・広告・ゲーム事業
+
+
+
+新卒採用情報
+先取り先行(終了済み)
+
+https://www.cyberagent.co.jp/careers/special/students/tech/
+
+
+
+コーポレートページ
+
+https://www.cyberagent.co.jp/
+
+
+
+
+
+mixi
+
+
+新卒採用情報
+https://mixi-recruit.snar.jp/jobboard/detail.aspx
+
+
+
+コーポレートページ
+https://mixi.co.jp/
+
+
+
+PLAID
+所在地が銀座。
+
+
+
+新卒採用情報
+https://recruit.plaid.co.jp/
+
+
+
+
+
+
+  </Box>
+  );
 }
+
+
+// const H1 = (props: {})
