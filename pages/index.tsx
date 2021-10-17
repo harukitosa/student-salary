b88393d5c73971bf1e7a30f15ecb157c0a4f1a77
masibw <masi19bw@gmail.com>
2021-10-17 23:13:34 +0900

    レビューを口コミ表記に統一

--- pages/index.tsx
+++ pages/index.tsx
@@ -249,7 +249,7 @@ function HomePage() {
           color={"white"}
           align={"center"}
         >
-          新着レビュー
+          新着口コミ
         </Text>
       </Box>
       <SimpleGrid columns={{ base: 1, md: 3 }} mt="4">
