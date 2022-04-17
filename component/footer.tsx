754bb1d388a71ee6871798ca7f3bd9fdb2b56d6f
harukitosa <13haruki28@gmail.com>
2022-04-17 17:09:36 +0900

    fix

--- component/footer.tsx
+++ component/footer.tsx
@@ -64,7 +64,9 @@ export const Footer = () => {
 
             <Stack align={"flex-start"}>
               <ListHeader>Contact</ListHeader>
-              <Link href={"https://twitter.com/tosa_now"}>@開発者とさ:twitter</Link>
+              <Link href={"https://twitter.com/tosa_now"}>
+                @開発者とさ:twitter
+              </Link>
             </Stack>
           </SimpleGrid>
         </Container>
