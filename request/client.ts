c08ca26027e153fcf9935ffe98d034a71a9a61fa
harukitosa <13haruki28@gmail.com>
2022-05-04 00:28:14 +0900

    fix (url)

--- request/client.ts
+++ request/client.ts
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 const link = createHttpLink({
   uri:
     process.env.NODE_ENV === "production"
-      ? "https://student-salary-api.an.r.appspot.com/query"
+      ? "https://studentsalaryprod.du.r.appspot.com/query"
       : "http://localhost:8080/query",
   credentials: "same-origin",
 });
