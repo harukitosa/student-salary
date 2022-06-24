117de02423485b6d5f3e8811bf4bc52c97d7b239
harukitosa <13haruki28@gmail.com>
2022-06-24 21:41:56 +0900

    fix

--- pages/api/salary.ts
+++ pages/api/salary.ts
@@ -13,7 +13,7 @@ const salary = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     ],
   });
-  console.log(salaryList)
+  console.log(salaryList);
   const response = JSON.stringify(
     salaryList,
     (key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
